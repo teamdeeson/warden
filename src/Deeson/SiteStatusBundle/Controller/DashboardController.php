b4ec9ddd84d858478e80ead8cc9e7225b747be07
Mike Davis <miked@deeson.co.uk>
2014-09-17 12:52:48 +0100

    Updated dashboard to list sites with errors

--- src/Deeson/SiteStatusBundle/Controller/DashboardController.php
+++ src/Deeson/SiteStatusBundle/Controller/DashboardController.php
@@ -10,41 +10,10 @@ class DashboardController extends Controller {
   public function indexAction() {
     /** @var SiteManager $manager */
     $manager = $this->get('site_manager');
-    $qb = $manager->createIndexQuery();
-    $qb->field('coreVersion.current')->notEqual('coreVersion.latestRelease');
-
-    $cursor = $qb->getQuery()->execute();
-
-    $sites = array();
-    foreach ($cursor as $result) {
-      $sites[] = $result;
-    }
-
-    //printf('<pre>%s</pre>', print_r($sites, true));
-//die();
-
-    $qb = $manager->createIndexQuery();
-    $qb->field('coreVersion.current')->notEqual('coreVersion.latestRelease');
-
-    $cursor = $qb->getQuery()->execute();
-
-    $siteModules = array();
-    foreach ($cursor as $result) {
-      $modules = array();
-      foreach ($result->getModules() as $module) {
-        if (!isset($module['latestVersion']) || $module['version'] == $module['latestVersion']) {
-          continue;
-        }
-        $modules[] = $module;
-      }
-      $siteModules[$result->getId()] = $modules;
-    }
-    //printf('<pre>%s</pre>', print_r($siteModules, true));
-    //die();
+    $sites = $manager->getAllSitesWithErrors();
 
     $params = array(
       'sites' => $sites,
-      'siteModules' => $siteModules,
     );
 
     return $this->render('DeesonSiteStatusBundle:Dashboard:index.html.twig', $params);
