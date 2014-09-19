62c843122ef5df9c7361498d910babdd00bd7198
Mike Davis <miked@deeson.co.uk>
2014-09-19 16:35:27 +0100

    Updated to add styling and logo image

--- scripts/clear-cache.sh
+++ scripts/clear-cache.sh
@@ -5,7 +5,16 @@ if [[ -n "$1" ]]; then
   ENV=$1
 fi
 
-php app/console cache:clear --env=${ENV:1} --no-debug
+RUNASAPACHE="No"
+if [[ -n "$2" ]]; then
+  RUNASAPACHE="Yes"
+fi
 
-# To run as apache user.
-#sudo su - apache -c "php $PWD/app/console cache:clear --env=${ENV:1} --no-debug"
+if [[ "${RUNASAPACHE}" == "Yes" ]]; then
+  # To run as apache user.
+  sudo su - apache -c "php $PWD/app/console --env=${ENV:1} cache:clear --no-debug"
+  sudo su - apache -c "php $PWD/app/console --env=${ENV:1} assets:install web"
+else
+  php app/console --env=${ENV:1} cache:clear --no-debug
+  php app/console --env=${ENV:1} assets:install web
+fi
