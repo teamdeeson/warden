a3277d3de9adb410714e7c222b781d6ce99058c8
John Ennew <johne@deeson.co.uk>
2015-01-03 12:20:36 +0000

    Major refactor of the Drupal site service to make it simpler and its functions more atomic

--- src/Deeson/WardenBundle/Services/WardenDrupalSiteService.php
+++ src/Deeson/WardenBundle/Services/WardenDrupalSiteService.php
@@ -3,53 +3,12 @@
 namespace Deeson\WardenBundle\Services;
 
 use Deeson\WardenBundle\Document\SiteDocument;
+use Deeson\WardenBundle\Event\SiteEvent;
 use Deeson\WardenBundle\Event\SiteUpdateEvent;
-use Deeson\WardenBundle\Exception\WardenRequestException;
-use Deeson\WardenBundle\Document\ModuleDocument;
-use Buzz\Browser;
-use Buzz\Exception\ClientException;
 use Deeson\WardenBundle\Managers\ModuleManager;
 use Symfony\Bridge\Monolog\Logger;
 
-class WardenDrupalSiteService extends BaseRequestService {
-
-  /**
-   * Drupal core version.
-   *
-   * @var float
-   */
-  protected $coreVersion = 0;
-
-  /**
-   * List of contrib modules.
-   *
-   * @var array
-   */
-  protected $moduleData = array();
-
-  /**
-   * @var \Deeson\WardenBundle\Document\SiteDocument $site
-   */
-  protected $site = NULL;
-
-  /**
-   * List of any additional errors that have come through from the site.
-   *
-   * @var array
-   */
-  protected $additionalIssues = array();
-
-  /**
-   * The site name from the site request.
-   *
-   * @var string
-   */
-  protected $siteName = '';
-
-  /**
-   * @var SSLEncryptionService
-   */
-  protected $sslEncryptionService;
+class WardenDrupalSiteService {
 
   /**
    * @var ModuleManager
@@ -57,148 +16,110 @@ class WardenDrupalSiteService extends BaseRequestService {
   protected $drupalModuleManager;
 
   /**
+   * @var SiteConnectionService
+   */
+  protected $siteConnectionService;
+
+  /**
+   * @var Logger
+   */
+  protected $logger;
+
+  /**
    * @param ModuleManager $drupalModuleManager
-   * @param SSLEncryptionService $sslEncryptionService
-   * @param Browser $buzz
+   * @param SiteConnectionService $siteConnectionService
+   * @param Logger $logger
    */
-  public function __construct(ModuleManager $drupalModuleManager, SSLEncryptionService $sslEncryptionService, Browser $buzz, Logger $logger) {
-    parent::__construct($buzz, $logger);
-    $this->sslEncryptionService = $sslEncryptionService;
+  public function __construct(ModuleManager $drupalModuleManager, SiteConnectionService $siteConnectionService, Logger $logger) {
     $this->drupalModuleManager = $drupalModuleManager;
-  }
-
-  /**
-   * @param SiteDocument $site
-   */
-  public function setSite(SiteDocument $site) {
-    $this->site = $site;
-  }
-
-  /**
-   * Get the core version for the site.
-   *
-   * @return float
-   */
-  public function getCoreVersion() {
-    return $this->coreVersion;
-  }
-
-  /**
-   * Get the modules data for the site.
-   *
-   * @return array
-   */
-  public function getModuleData() {
-    ksort($this->moduleData);
-    return $this->moduleData;
-  }
-
-  /**
-   * Get the site name for this site.
-   *
-   * @return string
-   */
-  public function getSiteName() {
-    return $this->siteName;
+    $this->siteConnectionService = $siteConnectionService;
+    $this->logger = $logger;
   }
 
   /**
    * Get the site status URL.
    *
+   * @param SiteDocument $site
+   *   The site being updated
+   *
    * @return mixed
    */
-  protected function getRequestUrl() {
-    return $this->site->getUrl() . '/admin/reports/warden';
+  protected function getSiteRequestUrl(SiteDocument $site) {
+    return $site->getUrl() . '/admin/reports/warden';
   }
 
   /**
-   * @return array
+   * Determine if the given site data refers to a Drupal site.
+   *
+   * @param SiteDocument $site
+   * @return bool
    */
-  public function getAdditionalIssues() {
-    return $this->additionalIssues;
+  protected function isDrupalSite(SiteDocument $site) {
+    // @TODO how to determine?
+    return TRUE;
   }
 
   /**
    * Processes the data that has come back from the request.
    *
-   * @param $wardenDataObject
-   *   Data that has come back from the request.
+   * @param SiteDocument $site
+   *   The site being updated
+   * @param $data
+   *   New data about the site.
    */
-  public function processRequestData($wardenDataObject) {
-    // @todo add logging of response to a file.
-    // Get the core version from the site.
-    if (isset($wardenDataObject->core->drupal)) {
-      $this->coreVersion = $wardenDataObject->core->drupal->version;
-    }
-    else {
-      foreach ($wardenDataObject->contrib as $module) {
-        $coreVersion = ModuleDocument::getMajorVersion((string) $module->version);
-        break;
-      }
-      $this->coreVersion = $coreVersion . '.x';
-    }
-
-    // Get the site name.
-    $this->siteName = $wardenDataObject->site_name;
-
-    //$this->coreVersion = isset($wardenDataObject->warden->core->drupal) ? $wardenDataObject->warden->core->drupal->version : '0';
-    $this->moduleData = json_decode(json_encode($wardenDataObject->contrib), TRUE);
+  public function processUpdate(SiteDocument $site, $data) {
+    $moduleData = json_decode(json_encode($data->contrib), TRUE);
+    $this->drupalModuleManager->addModules($moduleData);
+    $site->setName($data->site_name);
+    $site->setCoreVersion($data->core->drupal->version);
+    $site->setModules($moduleData, TRUE);
   }
 
   /**
-   * {@InheritDoc}
+   * Event: warden.site.refresh
+   *
+   * Fires when the Warden administrator requests for a site to be refreshed.
+   *
+   * @param SiteEvent $event
+   *   Event detailing the site requesting a refresh.
    */
-  public function processRequest() {
-    $this->setClientTimeout($this->connectionTimeout);
-
-    try {
-      $startTime = $this->getMicrotimeFloat();
-
-      // Don't verify SSL certificate.
-      // @TODO make this optional
-      $this->buzz->getClient()->setVerifyPeer(FALSE);
-
-      $url = $this->getRequestUrl();
-      $content = http_build_query(array('token' => $this->sslEncryptionService->generateRequestToken()));
-
-      /** @var \Buzz\Message\Response $response */
-      $response = $this->buzz->post($url, $this->connectionHeaders, $content);
-
-      if (!$response->isSuccessful()) {
-        $this->logger->addError("Unable to request data from {$url}\nStatus code: " . $response->getStatusCode() . "\nHeaders: " . print_r($response->getHeaders(), TRUE));
-        throw new WardenRequestException("Unable to request data from {$url}. Check log for details.");
-      }
-
-      $endTime = $this->getMicrotimeFloat();
-      $this->requestTime = $endTime - $startTime;
+  public function onRefreshSiteRequest(SiteEvent $event) {
+    $site = $event->getSite();
+    if (!$this->isDrupalSite($site)) {
+      return;
     }
-    catch (ClientException $e) {
-      throw new \Exception($e->getMessage());
-    }
+
+    $this->logger->addInfo('This is the start of a Drupal Site Refresh Event: ' . $site->getUrl());
+    $this->siteConnectionService->post($this->getSiteRequestUrl($site), $site);
+    $event->addMessage('A Drupal site has been updated: ' . $site->getUrl());
+    $this->logger->addInfo('This is the start of a Drupal Site Refresh Event: ' . $site->getUrl());
   }
 
   /**
+   * Event: warden.site.update
+   *
+   * Fires when a site is updated. This will detect if the site is a Drupal site
+   * and update the Drupal data accordingly.
+   *
    * @param SiteUpdateEvent $event
    */
   public function onWardenSiteUpdate(SiteUpdateEvent $event) {
-    $site = $event->getSite();
-    $data = $event->getData();
+    if (!$this->isDrupalSite($event->getSite())) {
+      return;
+    }
 
-    // @todo determine if this is a Drupal site.
+    $this->logger->addInfo('This is the start of a Drupal Site Update Event: ' . $event->getSite()->getUrl());
+    $this->processUpdate($event->getSite(), $event->getData());
+    $this->logger->addInfo('This is the end of a Drupal Site Update Event: ' . $event->getSite()->getUrl());
+  }
 
-    $this->setSite($site);
-    $this->processRequestData($data);
-    $siteName = $this->getSiteName();
-    $this->logger->addInfo('This is the start of the Drupal Site Event for ' . $siteName);
-
-    $moduleData = $this->getModuleData();
-    $this->drupalModuleManager->addModules($moduleData);
-    $additionalIssues = $this->getAdditionalIssues();
-    $coreVersion = $this->getCoreVersion();
-    $site->setName($siteName);
-    $site->setCoreVersion($coreVersion);
-    $site->setModules($moduleData, TRUE);
-    $site->setAdditionalIssues($additionalIssues);
-    $this->logger->addInfo('This is the end of the Drupal Site Event');
+  /**
+   * Get the current micro time.
+   *
+   * @return float
+   */
+  protected function getMicroTimeFloat() {
+    list($microSeconds, $seconds) = explode(' ', microtime());
+    return ((float) $microSeconds + (float) $seconds);
   }
 }
