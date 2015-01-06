16168fd10e9e171d3b939aeba5bb70278a30e7e3
John Ennew <johne@deeson.co.uk>
2015-01-06 10:35:05 +0000

    required environment in the update Drupal command

--- scripts/update-drupal.sh
+++ scripts/update-drupal.sh
@@ -1,8 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+if [[ -z "${1}" ]]; then
+  echo "Error: No environment variable provided! First argument must be @dev, @test or @prod."
+  exit 1;
+fi
 
 NEWONLY=""
-if [[ -n "$1" ]]; then
+if [[ -n "$2" ]]; then
   NEWONLY='--import-new'
 fi
 
-php app/console deeson:warden:drupal-update ${NEWONLY}
+php app/console deeson:warden:drupal-update --env=${ENV:1} ${NEWONLY}
