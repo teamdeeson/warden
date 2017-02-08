1a084b8cbda558ba0ad70693fe799262a074949c
Mike Davis <miked@deeson.co.uk>
2017-02-08 21:53:46 +0000

    fixed  #47 to detect security release versions between sites current module version and the latest version

--- scripts/update-drupal.sh
+++ scripts/update-drupal.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [[ -z "${1}" ]]; then
+NEWONLY=""
+ENV="$1"
+
+if [[ -z "${ENV}" ]]; then
   echo "Error: No environment variable provided! First argument must be @dev, @test or @prod."
+  usage
   exit 1;
 fi
 
-NEWONLY=""
 if [[ -n "$2" ]]; then
   NEWONLY='--import-new'
 fi
